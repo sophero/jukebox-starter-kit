9d03e7dc194e621f5549a0eb533373e6dbcfa1b2
Sophia Di <backwardsxmarathon@gmail.com>
2016-11-24 18:29:24 -0500

    Switched to using jukebox constructor. Currently non-functioning.

--- script.js
+++ script.js
@@ -1,33 +1,85 @@
+
 // Git requirements: 10+ commits, 3+ branches, 2+ issues.
 
-var Jukebox = {
-	songs: [],
-	audioPlayer: document.querySelector(".audio-player"),
-	playButton: document.querySelector(".play-button"),
-	pauseButton: document.querySelector(".pause-button"),
-	volumeUpButton: document.querySelector(".volume-up"),
-	volumeDownButton: document.querySelector(".volume-down"),
-	
-	play: function() {
+function jukeboxConstruct(songs) {
+	this.songs = songs;
+	this.addSong = function(newSong) {
+		this.songs = this.songs.push(newSong);
+	}
+	this.addSongArray = function(songArray) {
+		this.songs = this.songs.concat(songArray);
+	}
+	this.audioPlayer = document.querySelector(".audio-player");
+	this.playButton = document.querySelector(".play-button");
+	this.pauseButton = document.querySelector(".pause-button");
+	this.volumeUp = document.querySelector(".volume-up");
+	this.volumeDown = document.querySelector(".volume-down");
+
+	this.play = function() {
 		this.audioPlayer.play();
-	},
-	pause: function() {
-		this.audioPlayer.pause();
-	},
-	volumeUp: function() {
-		this.audioPlayer.volume += 0.1;
-	},
-	volumeDown: function() {
-		this.audioPlayer.volume -= 0.1;
 	}
 
-	// addSong: function(newSong) {
-	// 	songs.push(newSong);
-	// },
-	// addSongArray: function(songArray) {
-	// 	songs = songs.concat(songArray);
-	// }
+	this.pause = function() {
+		this.audioPlayer.pause()
+	}
+
+	this.playButton.addEventListener("click", this.play);
+	this.pauseButton.addEventListener("click", this.pause);
+
+// Jukebox.volumeUpButton.addEventListener("click", function() {
+// 	Jukebox.volumeUp();
+// });
+
+// Jukebox.volumeDownButton.addEventListener("click", function() {
+// 	Jukebox.volumeDown();
+// });
+
 }
+
+var Jukebox = new jukeboxConstruct();
+
+// var Jukebox = {
+// 	songs: [],
+// 	audioPlayer: document.querySelector(".audio-player"),
+// 	playButton: document.querySelector(".play-button"),
+// 	pauseButton: document.querySelector(".pause-button"),
+// 	volumeUpButton: document.querySelector(".volume-up"),
+// 	volumeDownButton: document.querySelector(".volume-down"),
+
+
+// 	play: function() {
+// 		this.audioPlayer.play();
+// 	},
+// 	pause: function() {
+// 		this.audioPlayer.pause();
+// 	},
+// 	volumeUp: function() {
+// 		this.audioPlayer.volume += 0.1;
+// 	},
+// 	volumeDown: function() {
+// 		this.audioPlayer.volume -= 0.1; 
+// 	}
+// }
+
+// Untested:
+// // loadSong: function(song) {
+
+// // }
+// changeSong: function(song) {
+// 	this.audioPlayer.src = song.url;
+// }
+// // listSongs: function() {
+// // 	for (var k = 0; k < this.songs.length; k++) {
+
+// // 	}
+// // }
+// addSong: function(newSong) {
+// 	this.songs.push(newSong);
+// },
+// addSongArray: function(songArray) {
+// 	this.songs = this.songs.concat(songArray);
+// }
+
 
 function Song(filename, artist, url) {
 	this.filename = filename;
@@ -38,25 +90,19 @@ function Song(filename, artist, url) {
 
 
 
-Jukebox.playButton.addEventListener("click", function() {
-	Jukebox.play();
-});
+// Jukebox.playButton.addEventListener("click", function() {
+// 	Jukebox.play();
+// });
 
-Jukebox.pauseButton.addEventListener("click", function() {
-	Jukebox.pause();
-});
+// Jukebox.pauseButton.addEventListener("click", function() {
+// 	Jukebox.pause();
+// });
 
-Jukebox.volumeUpButton.addEventListener("click", function() {
-	Jukebox.volumeUp();
-});
+// Jukebox.volumeUpButton.addEventListener("click", function() {
+// 	Jukebox.volumeUp();
+// });
 
-Jukebox.volumeDownButton.addEventListener("click", function() {
-	Jukebox.volumeDown();
-});
-
-// audioPlayer = document.querySelector(".audio-player");
-// playButton = document.querySelector(".play-button");
-// pauseButton = document.querySelector(".pause-button");
-// volumeUp = document.querySelector(".volume-up");
-// volumeDown = document.querySelector(".volume-down");
+// Jukebox.volumeDownButton.addEventListener("click", function() {
+// 	Jukebox.volumeDown();
+// });
 
