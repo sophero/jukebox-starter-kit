25a2a423d0c9fb8fc29a6cfc15e2a401828e8748
Sophia Di <backwardsxmarathon@gmail.com>
2016-11-24 21:50:35 -0500

    Rearranges code. Begins experiment with jukebox constructor initalisation functions for setting event listeners.

--- script.js
+++ script.js
@@ -1,104 +1,123 @@
 
 // Git requirements: 10+ commits, 3+ branches, 2+ issues.
 
+function Song(name, artist, url) {
+	this.name = name;
+	this.artist = artist;
+	this.url = url;
+}
+
 function jukeboxConstruct(songs) {
 	this.songs = songs;
-	this.addSong = function(newSong) {
-		this.songs = this.songs.push(newSong);
-	}
-	this.addSongArray = function(songArray) {
-		this.songs = this.songs.concat(songArray);
-	}
+
 	this.audioPlayer = document.querySelector(".audio-player");
 	this.playButton = document.querySelector(".play-button");
 	this.pauseButton = document.querySelector(".pause-button");
 	this.volumeUpButton = document.querySelector(".volume-up");
 	this.volumeDownButton = document.querySelector(".volume-down");
 
+	this.checkSongArray = function() {
+		if (typeof this.songs !== "array") {
+			this.songs = [];
+		}
+	}
+	this.checkSongArray();
+
+	this.initDOM = function() {
+		this.playButton.addEventListener("click", function() {
+			this.play();
+		})
+	}
+// Jukebox.playButton.addEventListener("click", function() {
+// 	Jukebox.play();
+// });
+// Jukebox.pauseButton.addEventListener("click", function() {
+// 	Jukebox.pause();
+// });
+// Jukebox.volumeUpButton.addEventListener("click", function() {
+// 	Jukebox.volumeUp();
+// });
+// Jukebox.volumeDownButton.addEventListener("click", function() {
+// 	Jukebox.volumeDown();
+// });
+
+
+	this.addSong = function(newSong) {
+		this.songs = this.songs.push(newSong);
+	}
+	this.addSongArray = function(songArray) {
+		this.songs = this.songs.concat(songArray);
+	}
+
 	this.play = function() {
 		this.audioPlayer.play();
 	}
-
 	this.pause = function() {
 		this.audioPlayer.pause()
 	}
-,
 	this.volumeUp = function() {
 		this.audioPlayer.volume += 0.1;
 	}
-
 	this.volumeDown = function() {
 		this.audioPlayer.volume -= 0.1;
 	}
+
+	this.changeSong = function(songToPlay) {
+		this.audioPlayer.src = '"' + songToPlay.url + '"';
+	}
+
+	this.loadSong = function(songUrl) {
+		// I'm not really sure what this means yet. 
+		// Need to create a playlist attribute and peripheral functions
+	}
+
+	this.listSongs = function() {
+		this.songNameArray = [];
+
+		for (var k = 0; k < this.songs.length; k++) {
+
+			songNameArray[k] = this.songs[k].artist + ' - ' 
+				+ this.songs[k].name;
+		}
+		return songNameArray;
+	}
 }
+
+// ----------------------------	//
+// 								//
+//  Instantiating the Jukebox 	//
+// 								//
+// ----------------------------	//
+
+
+// Something here like:
+// var userInput = 
 
 var Jukebox = new jukeboxConstruct();
 
-// var Jukebox = {
-// 	songs: [],
-// 	audioPlayer: document.querySelector(".audio-player"),
-// 	playButton: document.querySelector(".play-button"),
-// 	pauseButton: document.querySelector(".pause-button"),
-// 	volumeUpButton: document.querySelector(".volume-up"),
-// 	volumeDownButton: document.querySelector(".volume-down"),
+
+// Adding button functionality
+
+// Jukebox.playButton.addEventListener("click", function() {
+// 	Jukebox.play();
+// });
+// Jukebox.pauseButton.addEventListener("click", function() {
+// 	Jukebox.pause();
+// });
+// Jukebox.volumeUpButton.addEventListener("click", function() {
+// 	Jukebox.volumeUp();
+// });
+// Jukebox.volumeDownButton.addEventListener("click", function() {
+// 	Jukebox.volumeDown();
+// });
 
 
-// 	play: function() {
-// 		this.audioPlayer.play();
-// 	},
-// 	pause: function() {
-// 		this.audioPlayer.pause();
-// 	},
-// 	volumeUp: function() {
-// 		this.audioPlayer.volume += 0.1;
-// 	},
-// 	volumeDown: function() {
-// 		this.audioPlayer.volume -= 0.1; 
-// 	}
-// }
+// Loading songs
 
-// Untested:
-// // loadSong: function(song) {
+var meshuggahClockworks = new Song("Clockworks", "Meshuggah", "audio-files/01 Clockworks.mp3");
+var 
 
-// // }
-// changeSong: function(song) {
-// 	this.audioPlayer.src = song.url;
-// }
-// // listSongs: function() {
-// // 	for (var k = 0; k < this.songs.length; k++) {
+Jukebox.addSong();
+Jukebox.addSong(new Song("Specific Meaning in a Group of Dots", "Entheos", "audio-files/01 Specific Meaning in a Group of Dots.mp3"));
 
-// // 	}
-// // }
-// addSong: function(newSong) {
-// 	this.songs.push(newSong);
-// },
-// addSongArray: function(songArray) {
-// 	this.songs = this.songs.concat(songArray);
-// }
-
-
-function Song(filename, artist, url) {
-	this.filename = filename;
-	this.artist = artist;
-	this.url = url;
-}
-
-
-
-
-Jukebox.playButton.addEventListener("click", function() {
-	Jukebox.play();
-});
-
-Jukebox.pauseButton.addEventListener("click", function() {
-	Jukebox.pause();
-});
-
-Jukebox.volumeUpButton.addEventListener("click", function() {
-	Jukebox.volumeUp();
-});
-
-Jukebox.volumeDownButton.addEventListener("click", function() {
-	Jukebox.volumeDown();
-});
-
+Jukebox.changeSong()
